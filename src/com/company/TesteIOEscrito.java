50df9152131f2e1696d502bbba84c1f33b3c03bf
Gian Lucas Alves Ferreira <gianlol201@gmail.com>
2022-05-18 17:44:20 -0300

    :+1:

--- src/com/company/TesteIOEscrito.java
+++ src/com/company/TesteIOEscrito.java
@@ -2,29 +2,28 @@ package com.company;
 
 import java.io.*;
 
-public class TesteInputOutput {
+public class TesteIOEscrito {
     public static void main(String[] args) throws IOException {
-
         //le meu arquivo
-        InputStream       fis = new FileInputStream("lorem.txt");
+        InputStream fis       = System.in;
         InputStreamReader isr = new InputStreamReader(fis);
-        BufferedReader    br  = new BufferedReader(isr);
+        BufferedReader br     = new BufferedReader(isr);
 
         //escreve no arquivo
-        OutputStream      fos = new FileOutputStream("lorem3.txt");
+        OutputStream fos      = System.out; // Joga no console
         Writer            osw = new OutputStreamWriter(fos);
         BufferedWriter    bw  = new BufferedWriter(osw);
 
         String linha = br.readLine();
 
-        while (linha != null){
+        while (linha != null && !linha.isEmpty()){
             bw.write(linha);
             bw.newLine();
+            // bw.flush(); //ele joga cada linha apos terminar no consoloe
             linha = br.readLine();
 
         }
         br.close();
         bw.close();
-
     }
 }
